796c7b09c526bd67e0412a3f235643e0065916d8
Jose Tapia <jose.tapia@ki.se>
2018-08-31 17:03:09 +0200

    First commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://newsapi.org/v2',
+  apiKey: 'eb7c78775b92481289d9f83bbb5ce494'
 };
 
 /*
